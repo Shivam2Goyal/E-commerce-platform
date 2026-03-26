a0499db4dd43a0af549cd49b1cd35486c7ec873b
Shivam <sg5237757@gmail.com>
2026-03-27 01:03:10 +0530

    updated contacts

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,28 +1,51 @@
 import React from 'react'
 import Title from '../components/Title'
-import { assets } from '../assets/assets'
 import NewsletterBox from '../components/NewsletterBox'
-const contact = () => {
+
+const Contact = () => {
   return (
     <div>
       <div className='text-center text-2xl pt-10 border-t'>
-        <Title text1={'CONTACT'} text2={' US'}/>
+        <Title text1={'CONTACT'} text2={' US'} />
       </div>
 
-      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
-        <img src={assets.contact_img} className='w-full md:max-w-[480px]' alt="" />
-        <div className='flex flex-col justify-center items-start gap-6'>
-          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>671314 Jodhpur <br /> Rajasthan, Indian</p>
-          <p className='text-gray-500'> Tel: 7994655634 <br />Email: gouthamas369@gmail.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
-          <p className='text-gray-500'> Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+      {/* Centered Content */}
+      <div className='my-10 flex justify-center mb-28'>
+        <div className='flex flex-col gap-8 max-w-2xl text-center'>
+
+          <div>
+            <p className='font-semibold text-xl text-gray-700'>Our Store</p>
+            <p className='text-gray-500 mt-2'>
+              671314 Jodhpur <br /> Rajasthan, India
+            </p>
+          </div>
+
+          <div>
+            <p className='text-gray-500'>
+              Tel: 9294531766 / 7289086751 <br />
+              Email: clshivam16@gmail.com / mvdikshant@gmail.com
+            </p>
+          </div>
+
+          <div>
+            <p className='font-semibold text-xl text-gray-700'>Careers</p>
+            <p className='text-gray-500 mt-2'>
+              Learn more about our team and current opportunities.
+            </p>
+          </div>
+
+          <div>
+            <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>
+              Explore Jobs
+            </button>
+          </div>
+
         </div>
       </div>
-      <NewsletterBox/>
+
+      <NewsletterBox />
     </div>
   )
 }
 
-export default contact
+export default Contact
